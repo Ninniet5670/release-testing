68b9c01390cf14a6b730d6ed9e3fef057b095256
Ninniet5670 <marconeto3000@gmail.com>
2024-04-11 00:24:59 -0300

    more time to admire my love

--- src/testing/test.py
+++ src/testing/test.py
@@ -7,5 +7,7 @@ def love_Izabela():
     print('I love you my love, you are everything to me Sz Izabela')
 
 if __name__ == '__main__':
+    hello_testing()
+    sleep(3)
     love_Izabela()
     sleep(3)
